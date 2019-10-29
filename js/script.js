e40963349fcc4b29e809498bf99a83c34ee834f7
ruslanA.uwk@gmail.com <ruslanA.uwk@gmail.com>
2019-10-29 20:57:53 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+localStorage.setItem( '1', { id: 1, name: "book 1", author: "author 1" } );
+localStorage.setItem( '2', {id: 2, name: "book 2", author: "author 2"});
+
+$(document).ready(function(){
+
+    for (let index = 0; index < localStorage.length; index++) {
+        for (const key in object) {
+            if (object.hasOwnProperty(key)) {
+                const element = object[key];
+                
+            }
+        }
+        
+    }
+
+    
+})
